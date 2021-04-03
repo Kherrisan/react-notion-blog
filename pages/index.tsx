6f84662c2acc550cc05f3fea2bd2d46aaec4aeae
kherrisan <zdkscope@qq.com>
2021-04-03 20:36:55 +0800

    vercel 没反应？

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,7 +118,7 @@ const HomePage = ({ posts }: { posts: Post[] }) => {
             </div>
             <div className="mt-8 text-2xl font-bold">Kherrisan 的博客</div>
             <div className="mt-2 text-gray-400">
-              🍯{' '}来点蜂蜜
+              🍯{' '}来点蜂蜜吧
               {/* <Link href="/friends">
                 <a className="text-purple-400 hover:bg-purple-100 p-1 rounded">
                 </a>
